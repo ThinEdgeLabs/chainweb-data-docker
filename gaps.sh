e5df890352f2850ba12494bc22e2e1f3eeff54bd
Radu Ciobanu <radu.ciobanu22@gmail.com>
2022-07-20 17:34:07 +0800

    Added params for chainweb host, ports in gaps.sh

--- gaps.sh
+++ gaps.sh
@@ -6,6 +6,6 @@ fi
 
 date_timestamp=$(date '+%Y-%m-%d %H:%M:%S')
 echo -e "Fill was started at $date_timestamp" >> /tmp/fill_history.log
-/usr/local/bin/chainweb-data fill --service-host=172.15.0.1 --p2p-host=172.15.0.1 --service-port=31351 --p2p-port=31350 --dbuser=postgres --dbpass=postgres --dbname=postgres
+/usr/local/bin/chainweb-data fill --service-host=$CHAINWEB_NODE_HOST --p2p-host=$CHAINWEB_NODE_HOST --service-port=$CHAINWEB_NODE_SERVICE_PORT --p2p-port=$CHAINWEB_NODE_P2P_PORT --dbuser=postgres --dbpass=postgres --dbname=postgres
 date_timestamp=$(date '+%Y-%m-%d %H:%M:%S')
 echo -e "Fill was ended at $date_timestamp" >> /tmp/fill_history.log
